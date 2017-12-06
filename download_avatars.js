f9cec6bb02f1dc85eee50bf30e8a286a2e16d3b2
Mel Smith <melpcsmith@gmail.com>
2017-12-06 07:32:22 +0000

    github avatar downloader fully tested and functioning

--- download_avatars.js
+++ download_avatars.js
@@ -1,16 +1,43 @@
 var request = require('request');
+var fs = require('fs');
+const repoOwner = process.argv[2];
+const repoName = process.argv[3];
+
+
+var requestURL = {
+  url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+  headers: {
+    'User-Agent': 'request',
+  }
+};
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+// console.log('Make sure you type in the name of the Contributor and then the Repo name.')
 
-function getRepoContributors(repoOwner, repoName, cb) {
-  var options = {
-    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-    headers: {
-      'User-Agent': 'request'
+function getRepoContributors(requestURL, cb) {
+  var url = {}
+  request(requestURL, function(err, res, body) {
+    var array = JSON.parse(body);
+    if(!fs.existsSync("./avatars/")) {
+      fs.mkdirSync("./avatars/")
     }
-  };
-
-  request(options, function(err, res, body) {
-    cb(err, body);
-  });
+    array.forEach(function(entry){
+      url.username = entry.login;
+      url.avatar = entry.avatar_url;
+      cb(url)
+    })
+  })
 }
+
+function downloadImageByUrl(url) {
+  request.get(url.avatar)
+  .on('error', function (err){
+    throw err;
+  })
+  .on('end', function (end){
+    console.log("Downloaded")
+  })  
+  .pipe(fs.createWriteStream('./avatars/' + url.username + '.jpg'))
+}
+
+getRepoContributors(requestURL, downloadImageByUrl) 
